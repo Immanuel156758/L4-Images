edc13192f422d4de5d4db8bfb2c79c7e3896a995
Immanuel156758 <161232399+Immanuel156758@users.noreply.github.com>
2024-11-17 16:07:31 +0100

    Add files via upload

--- L4.py
+++ L4.py
@@ -9,27 +9,30 @@ y = 50
 p = 0
 h = 0
 t = 0
+bg_color = "blue"
 
 alien = Actor("alien")
 title = "Click on the alien"
 percentage = ""
 def on_mouse_down(pos):
-	global title, h, p, t
+	global title, h, p, t, bg_color
 
 	if alien.collidepoint(pos):
 		alien.x = random.randint(50,450)
 		alien.y = random.randint(50,450)
 		title = "You hit"
+		bg_color = "green"
 		t += 1
 		h += 1
 		p = (h/t)*100
 	else:
 		title = "You missed"
+		bg_color = "red"
 		t += 1
 		p = (h/t)*100
 
 def draw():
-	screen.fill("blue")
+	screen.fill(bg_color)
 	alien.draw()
 	screen.draw.text(title,(x,y))
 	screen.draw.text(percentage+str(p)+"%",(400,50))
